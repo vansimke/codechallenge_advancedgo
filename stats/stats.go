5d85109134814bcae8c872816838fb71f9e436f5
Michael Van Sickle <mvansickle1@gmail.com>
2020-11-10 14:08:45 -0500

    initial commit of challenge four code

--- stats/stats.go
+++ stats/stats.go
@@ -1,6 +1,12 @@
 package stats
 
-import "strings"
+import (
+	"log"
+	"strings"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
 
 type Stats struct{}
 
@@ -17,11 +23,33 @@ func (s Stats) TotalWords(lines []string) int {
 }
 
 func (s Stats) WordFrequency(lines []string) map[string]int {
+	lineCh := make(chan string)
+	m := sync.Mutex{}
 	var result = make(map[string]int)
-	for _, line := range lines {
-		for _, word := range strings.Fields(line) {
-			result[word]++
-		}
+
+	g := new(errgroup.Group)
+
+	for i := 0; i < 10; i++ {
+		g.Go(func() error {
+			for line := range lineCh {
+				for _, word := range strings.Fields(line) {
+					m.Lock()
+					result[word]++
+					m.Unlock()
+				}
+			}
+			return nil
+		})
 	}
+
+	for _, line := range lines {
+		lineCh <- line
+	}
+	close(lineCh)
+	err := g.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return result
 }
